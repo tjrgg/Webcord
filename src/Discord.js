ea547a1f4e471311475239fd191c53c43b5f3fde
HyperCoder <bryan.waynejr@outlook.com>
2016-10-03 22:48:42 -0400

    Work on stuff

--- src/Discord.js
+++ src/Discord.js
@@ -1,5 +1,6 @@
 const Request = require("request");
 const Constants = require("./util/Constants");
+const Webhook = require("./Webhook");
 
 class Discord {
     constructor(token) {
@@ -15,13 +16,31 @@ class Discord {
                     "Authorization": this.token,
                     "Content-Type": "application/json"
                 },
-                "body": {
+                "body": JSON.stringify({
                     "name": name,
                     "avatar": avatar
+                })
+            }, (error, response, body) => {
+                if (error) return reject({error: error, response: response, body: body});
+                return resolve(response, body);
+            });
+        });
+    }
+
+    getWebhooks(channelID) {
+        return new Promise((resolve, reject) => {
+            Request({
+                "method": "GET",
+                "url": Constants.Endpoints.get(channelID),
+                "headers": {
+                    "Authorization": this.token,
+                    "Content-Type": "application/json"
                 }
             }, (error, response, body) => {
-                if (error) return reject(error, response, body);
-                return resolve(response, body);
+                if (error) return reject({error: error, response: response, body: body});
+                let webhooks = JSON.parse(body);
+                if (webhooks.code) return reject({response: response, body: body});
+                return resolve(webhooks.map(webhook => new Webhook(webhook)));
             });
         });
     }
