b3b6f368779873cb99efd6fdc97eea27a87f11e7
HyperCoder <bryan.waynejr@outlook.com>
2016-10-05 22:50:16 -0400

    More files

--- src/Util/Endpoints.js
+++ src/Util/Endpoints.js
@@ -2,5 +2,6 @@ const baseUrl = `https://discordapp.com/api`;
 
 const Endpoints = module.exports = {
     webhooks: channel => `${baseUrl}/channels/${channel}/webhooks`,
-    webhook: (id, token) => `${baseUrl}/webhooks/${id}/${token}`
+    webhook: (id, token) => `${baseUrl}/webhooks/${id}/${token}`,
+    webhook_slack: (id, token) => `${baseUrl}/webhooks/${id}/${token}/slack`
 };
--- src/Util/Webhook.js
+++ src/Util/Webhook.js
@@ -34,6 +34,54 @@ class Webhook {
             });
         });
     }
+
+    slack(content = {}) {
+        return new Promise((resolve, reject) => {
+            Request({
+                "method": "POST",
+                "url": Endpoints.webhook_slack(this.id, this.token),
+                "headers": {
+                    "Content-Type": "application/json"
+                },
+                "body": JSON.stringify(content)
+            }, (error, response, body) => {
+                if (error) return reject({error: error, response: response, body: body});
+                return resolve({response: response, body: body});
+            });
+        });
+    }
+
+    manage(options = {}) {
+        if (!options.name) options.name = this.name;
+        return new Promise((resolve, reject) => {
+            Request({
+                "method": "PATCH",
+                "url": Endpoints.webhook(this.id, this.token),
+                "headers": {
+                    "Content-Type": "application/json"
+                },
+                "body": JSON.stringify(options)
+            }, (error, response, body) => {
+                if (error) return reject({error: error, response: response, body: body});
+                return resolve({response: response, body: body});
+            });
+        });
+    }
+
+    delete() {
+        return new Promise((resolve, reject) => {
+            Request({
+                "method": "DELETE",
+                "url": Endpoints.webhook(this.id, this.token),
+                "headers": {
+                    "Content-Type": "application/json"
+                }
+            }, (error, response, body) => {
+                if (error) return reject({error: error, response: response, body: body});
+                return resolve({response: response, body: body});
+            });
+        });
+    }
 }
 
 module.exports = Webhook;
